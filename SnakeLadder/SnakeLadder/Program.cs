54ae91ccebc471fd33012ed50ab79a59d1beea48
SakshiKorgaonkar <korgaonkarsakshi23@gmail.com>
2024-04-20 14:46:11 +0530

    [sakshi]add:snake ladder uc1 done

--- SnakeLadder/SnakeLadder/Program.cs
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace SnakeLadder
+{
+    internal class Program
+    {
+        int initial;
+
+        Program()
+        {
+            this.initial = 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Program program = new Program();
+            program.initial = random.Next(1,6);
+            Console.WriteLine("Number generated : "+ program.initial);
+        }
+    }
+}
--- SnakeLadder/SnakeLadder/obj/Debug/net6.0/SnakeLadder.AssemblyInfo.cs
+++ SnakeLadder/SnakeLadder/obj/Debug/net6.0/SnakeLadder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SnakeLadder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0039faee6374f08d6b37c3f6c91c45ab79146b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SnakeLadder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SnakeLadder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
